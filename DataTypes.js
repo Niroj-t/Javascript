df385c56d79b3b81dfbe1a88a70ae2299e06a614
Niroj_t <nirojthapa661@gmail.com>
2025-02-16 23:06:16 +0545

    Javascript DataTypes

---
+++ DataTypes.js
@@ -0,0 +1,18 @@
+//datatypes
+/let age = 25;
+age = true;
+console.log(age);
+
+//let age;
+console.log(age); // undefined
+
+let age = null;
+console.log(age); // null
+
+let name = 'Niroj';
+console.log(typeof(name)); 
+
+let name = 'niroj';
+let UpperName = name. toUpperCase();
+console.log(UpperName);
+))
