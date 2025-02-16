7ca71b816e06478cbe44dc5b5b0ee989d9516df3
Niroj_t <nirojthapa661@gmail.com>
2025-02-16 23:02:12 +0545

    Javascript Variables

---
+++ Variables.js
@@ -0,0 +1,44 @@
+//creating variables using 'var'
+var x=25;
+console.log(x);
+
+//example of function scoped
+function demo()
+{
+    var name = "Niroj";
+    console.log(name);//function scoped
+}
+demo();
+console.log(name); //throw error
+
+//re-definition using var
+var x = 20;
+var y = 30;
+
+//creating variables using 'let'
+let y = 10;
+console.log(y);
+
+{
+let y = 5;
+console.log(y);//blocked scope
+}
+console.log(y); // throw error
+
+//re-definition using let
+let y = 2;
+let y = 3;
+console.log(y); // throw eeror
+
+let string = 'Niroj';
+string = '25';
+console.log(string);
+
+creating variables using 'const'
+const roll=30;
+console.log(roll);
+
+value not changed
+const age = 40;
+age = 40;
+console.log(age); // throw error
