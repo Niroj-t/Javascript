0b71cea8be3fcd43ff81bdff44981086a5b61a55
Niroj_t <nirojthapa661@gmail.com>
2025-02-17 21:10:38 +0545

    Javascript Loops

---
+++ Loops.js
@@ -0,0 +1,63 @@
+//for loop
+/*
+for(let i = 1; i <= 5; i++){
+    console.log("Javascript");
+}
+
+//counting print
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}
+//reverse Counting
+for(let i = 10; i >=0; i--){
+    console.log(i);
+}
+
+//break keyword
+for(let i=1; i<=6;i++)
+{
+    if(i==5){
+        break;
+    }
+    else{
+    console.log(i);
+    }
+}
+//continue keyword
+for(let i=1; i<=5; i++){
+    if(i==2){
+        continue;
+    }
+    else{
+        console.log(i);
+    }
+}
+
+//while loop
+let sum = 0;
+let i = 1;
+while(i<10){
+    sum = sum + i;
+    i++;
+}
+console.log(sum);
+
+//infinite loop
+let i =1;
+while(i<5){
+    if(i==3){
+        //i++ - solution
+        continue;
+    }
+    else{
+        console.log(i);
+        i++;
+    }
+}
+*/
+//do while loop
+let i = 1;
+do{
+    console.log(i);
+    i++
+}while(i<10);
