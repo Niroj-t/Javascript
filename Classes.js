b453d52ef6ae8b75a16f5e08afc5186427492c97
Niroj_t <nirojthapa661@gmail.com>
2025-02-24 09:29:35 +0545

    Javascript classes Added

---
+++ Classes.js
@@ -0,0 +1,34 @@
+/*
+//creating class
+class student{
+    //properties
+    name = "Niroj";//public
+    #age = 22;//private
+    roll = 70;
+    //behaviour
+    study(){
+        console.log("I am studying",this.#age);//accessing private member using this keyword
+    }
+
+}
+const obj = new student();
+console.log(obj.name);
+obj.study();
+*/
+
+//Getter and Setter
+class GetSet{
+    #msg = "Hello, Niroj";
+    get msg(){
+        return this.#msg;
+    }
+    set msg(val){
+        this.#msg = `Hello,${val}`;
+    }
+}
+const obj = new GetSet();
+console.log(obj.msg);
+obj.msg = "cake";
+console.log(obj.msg);
+
+
