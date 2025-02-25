9c10d5f9175e7d033dafa8b37fef3b17854539a5
Niroj_t <nirojthapa661@gmail.com>
2025-02-25 21:07:38 +0545

    Javascript ErrorHandling Added

---
+++ ErrorHandling.js
@@ -0,0 +1,21 @@
+//Error Handling
+try{
+    console.log("This is try block");
+    console.log(x);
+    console.log("try block ends here")
+}
+catch(e){
+    console.log("This is catch block");
+    console.log("Your error is here", e);
+}
+finally{
+    console.log("Always execute")
+}
+
+// create custom error
+try{
+    console.log(x);//reference error
+}
+catch(e){
+    throw new Error("Please Declared first and print");
+}
