4b701be3708edf943f60ab6d5ab131dde26310e7
Niroj_t <nirojthapa661@gmail.com>
2025-02-25 19:28:39 +0545

    Javascript objects updated

--- Objects.js
+++ Objects.js
@@ -34,9 +34,51 @@ function CreateStudent(){
     this.age = 22
 
 }
-
 let student = new CreateStudent();
-
 console.log(student);
 
+//Dynamic object
+let obj = {
+    name : "Niroj",
+    age : 20,
+    height : 180
+};
+console.log(obj.age);
+obj.color = "Red";//add value during run time
+console.log(obj);
+
+//object cloning using spread operator(...)
+let src = {
+    age : 20,
+    name : "Niroj",
+    height : 179
+};
+let des = {...src};
+console.log("src:", src);
+console.log("des:", des);
+
+//object cloning using assign operator
+let src1 = {
+    age : 22,
+    name : "Niroj",
+    height : 185
+};
+let des1 = Object.assign({}, src1);
+console.log("src:", src1);
+console.log("des:", des1);
+
+//object cloning using iteration
+let src2 = {
+    age : 22,
+    name : "Niroj",
+    height : 185
+};
+let des2 = {};
+for(let key in src1){
+    let newkey = key;
+    let newvalue = src[key];
+    des2[newkey] = newvalue;
+}
+console.log("src :", src2);
+console.log("des:", des3);
 
