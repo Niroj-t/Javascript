1a4cb55a4a28838239366da7c1eee65fe1e21860
Niroj_t <nirojthapa661@gmail.com>
2025-02-28 13:05:34 +0545

    create, modify and delete the HTML elements

---
+++ DOM Manupulation/script.js
@@ -0,0 +1,49 @@
+/*
+//accesing elements by id
+const box1=document.getElementById("box-1");
+console.log(box1);
+
+//accesing elements by tag name
+const divs=document.getElementsByTagName("div");
+console.log(divs);
+
+
+//accesing elements by class name
+const cls=document.getElementsByClassName("box");
+console.log(cls);
+
+
+//query selector
+const que = document.querySelector('.container #box-2');
+console.log(que);
+//query selector all
+const queAll = document.querySelectorAll('.box');
+console.log(queAll);
+
+//Modify HTML elements
+const box1= document.getElementById('box-1');
+box1.innerHTML="<p>Hello</p>"
+
+const boxes = document.getElementsByClassName('box');
+for(let i=0; i< boxes.length; i++){
+    boxes[i].classList.add('round-border');
+}
+
+//Remove Elements
+box1.classList.remove('box');
+*/
+
+//create element
+const newPara = document.createElement("p");
+newPara.innerText = "This is the new paragraph";
+
+const container = document.getElementById('container');
+container.appendChild(newPara);
+
+
+
+
+
+
+
+ 
