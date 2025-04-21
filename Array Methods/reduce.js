f72c35c443f10fc8cdb07baab1a91aa0be25bfd6
Niroj_t <nirojthapa661@gmail.com>
2025-04-21 15:46:03 +0545

    Reduce Array Method

---
+++ Array Methods/reduce.js
@@ -0,0 +1,17 @@
+
+//Example 1
+let arr=[1,2,3,4];
+let arr2 = arr.reduce((sum,num)=>{
+    return sum+num;
+})
+console.log(arr2);
+
+//Example 2
+const cart = [
+    {item:'watch',price:1500},
+    {item:'Pen',price:10},
+    {item:'Book',price:450},
+    {item:'T-Shirt',price:1200}
+];
+const total = cart.reduce((sum, product) => sum + product.price, 0);
+console.log(total);
